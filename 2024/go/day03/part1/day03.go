a863fd24efec9f8f5fd68297037744d18ea987ce
Bill Allaire <mytec@me.com>
2024-12-03 21:42:01 -0500

    Initial Day 3 Part 1

---
+++ 2024/go/day03/part1/day03.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+// 183,380,722
+
+func main() {
+	file, err := os.Open("./puzzle_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var sum = 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches, err := returnMatches(line)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for _, match := range matches {
+			left, err := strconv.Atoi(match[1])
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			right, err := strconv.Atoi(match[2])
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			sum += left * right
+		}
+	}
+
+	fmt.Printf("Sum: %v\n", sum)
+}
+
+func returnMatches(line string) ([][]string, error) {
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	matches := re.FindAllStringSubmatch(line, -1)
+	if matches == nil {
+		return [][]string{}, errors.New("no matches found")
+	}
+
+	return matches, nil
+}
